a64a6bd1dca5b3f82bfd58dd7a90b12da4666b2a
Max Baraitser Smith <m.baraitsersmith@gmail.com>
2024-12-12 16:12:06 +0100

    com

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # hacktalking_janusgraph
+
+#first time startup:
+docker run -it -p 8182:8182 --name janusgraph-container -v ~:/opt/janusgraph/exports --name janusgraph-container janusgraph/janusgraph
+
+#then in another window:
+#docker exec -u 0:0 janusgraph-container chown -R janusgraph /opt/janusgraph/exports
--- run_janusgraph_docker.sh
+++ run_janusgraph_docker.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-
-#####figure out how to start and stop docker properly!
-
-exportpath_host="/home/max/exporttest"
-
-#docker start -it -p 8182:8182 --name janusgraph-container -v $exportpath_host:/opt/janusgraph/export281 --name janusgraph-container janusgraph/janusgraph
-
-#first time startup:
-docker run -it -p 8182:8182 --name janusgraph-container -v $exportpath_host:/opt/janusgraph/export281 --name janusgraph-container janusgraph/janusgraph
-
-#then in another window:
-#docker exec -u 0:0 janusgraph-container chown -R janusgraph /opt/janusgraph/export281
+sudo docker start janusgraph-container
